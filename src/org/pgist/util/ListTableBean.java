14532defcdd05d87742c12ef03d2445df57c1efc
kenny <>
2005-07-31 13:07:53 +0000

    *** empty log message ***

--- src/org/pgist/util/ListTableBean.java
+++ src/org/pgist/util/ListTableBean.java
@@ -1,5 +1,9 @@
 package org.pgist.util;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.component.UIData;
 import javax.faces.component.UIInput;
 import javax.faces.component.UISelectBoolean;
@@ -53,4 +57,33 @@ public class ListTableBean {
     }
     
     
+    public Long selectedId() {
+        return (Long) objectId.getValue();
+    }//getSelectedId()
+    
+    
+    public List selectedIds(Class rowClass, String property) {
+        List idList = new ArrayList();
+        
+        try {
+            StringBuffer methodName = new StringBuffer("get");
+            methodName.append(property.toUpperCase().charAt(0)).append(property.substring(1));
+            Method method = rowClass.getMethod(methodName.toString());
+            
+            int n = data.getRowCount();
+            for (int i = 0; i < n; i++) {
+                data.setRowIndex(i);
+                if (checked.isSelected()) {
+                    idList.add(method.invoke(data.getRowData()));
+                }
+            }//for i
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        
+        return idList;
+    }//selectedIds()
+    
+    
 }//class ListTableBean
+
