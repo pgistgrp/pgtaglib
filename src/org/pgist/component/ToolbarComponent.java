8aff552682c26be33b2fa6e73effd5e85735dd6e
kenny <>
2005-08-04 21:33:20 +0000

    *** empty log message ***

--- src/org/pgist/component/ToolbarComponent.java
+++ src/org/pgist/component/ToolbarComponent.java
@@ -1,13 +1,10 @@
 package org.pgist.component;
 
-import javax.faces.component.StateHolder;
+import java.util.Iterator;
+
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIComponentBase;
 import javax.faces.context.FacesContext;
-import javax.faces.event.FacesEvent;
-import javax.faces.event.FacesListener;
-
-import java.util.Iterator;
 
 
 public class ToolbarComponent extends UIComponentBase {
